1c6618ec1752de7cac6230d4ffd4485514ac9a64
simonef <simonefagundessilva2606@gmail.com>
2024-04-25 11:46:01 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,42 @@
+const apiKey = "0a88c29754fbfb6cdfec7cc829d20986";
+const apiCountryURL = "https://flagsapi.com/BR/shiny/64.png";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+const weatherContainer = document.querySelector("#weather-data");
+//Funções
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    return data;
+
+};
+
+const showWeatherData = async(city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+    weatherContainer.classList.remove("hide");
+};
+//Eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+});
